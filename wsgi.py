4fd8097a395ce9e93d9513201bacfd6c3e8d4f21
Hao Lee <haolee.swjtu@gmail.com>
2016-06-20 21:22:08 +0800

    输入纯中文出错是因为经过正则提取后得到的列表为空，转换得到的字符串也为空，MBSP处理空串会报错。现在已经通过if语句来保证传递给MBSP的字符串绝对不为空。

--- wsgi.py
+++ wsgi.py
@@ -167,13 +167,14 @@ def analyzer(sourcestring):
 	#if not m and  not n and len(i)>4:
 	if not m and  not n:
 	    sourcelist_tmp.append(sourceword)
+    #现在单词初步处理完成
     sourcelist = sourcelist_tmp
-    #词形还原
+    #进行词形还原。到这里为止，sourcelist里面已经都是英文单词，没有乱七八糟的表标点符号等字符，不会导致MBSP出错了。
     if ostype == "Windows":
 	#Windows上MBSP没法用，所以本地测试时不进行lemmatize
 	pass
     else:
-	#lemmatization，传入list，返回值也是list，只不过词形被还原了
+	#lemmatization，传入list，返回值也是list（只不过词形被还原了）
 	sourcelist = lemmatizer.lemmatizer(sourcelist)    
     #最终结果为有序字典result，便于后期对字典排序
     result = collections.OrderedDict()
